87202cec074c0423d68a6177ee21829ff2f4640b
Paper Benni <paperbenni@gmail.com>
2019-03-07 20:22:00 +0100

    push stack patch

--- config.h
+++ config.h
@@ -71,6 +71,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = {"dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL};
 static const char *termcmd[] = {"st", NULL};
 
+#include "push.c"
+
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{MODKEY, XK_p, spawn, {.v = dmenucmd}},
@@ -78,6 +80,8 @@ static Key keys[] = {
 	{MODKEY, XK_b, togglebar, {0}},
 	{MODKEY, XK_j, focusstack, {.i = +1}},
 	{MODKEY, XK_k, focusstack, {.i = -1}},
+	{MODKEY|ControlMask, XK_j, pushdown, {0} },
+	{MODKEY|ControlMask, XK_k, pushup, {0} },
 	{MODKEY, XK_i, incnmaster, {.i = +1}},
 	{MODKEY, XK_d, incnmaster, {.i = -1}},
 	{MODKEY, XK_h, setmfact, {.f = -0.05}},
