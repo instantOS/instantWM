5ad41ce6190ca173a20603910c1098408ff3fbb1
Paper Benni <paperbenni@gmail.com>
2019-03-08 15:18:22 +0100

    three column layout

--- config.h
+++ config.h
@@ -44,11 +44,13 @@ static const float mfact = 0.55;  /* factor of master area size [0.05..0.95] */
 static const int nmaster = 1;	 /* number of clients in master area */
 static const int resizehints = 1; /* 1 means respect size hints in tiled resizals */
 
+#include "tcl.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "|||",      tcl },
 	{ NULL,       NULL },
 };
 
