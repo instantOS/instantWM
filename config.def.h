7b300130c2dbd97a24e878c75a7b7086d057e77f
paperbenni <paperbenni@gmail.com>
2019-03-10 16:27:39 +0100

    remove firefox rule

--- config.def.h
+++ config.def.h
@@ -36,7 +36,6 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{"Gimp", NULL, NULL, 0, 1, -1},
-	{"Firefox", NULL, NULL, 1 << 8, 0, -1},
 };
 
 /* layout(s) */
