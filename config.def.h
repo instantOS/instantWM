0fc9006da143f1ad35f76c00506512f810625c0f
paperbenni <paperbenni@gmail.com>
2019-05-16 22:27:46 +0200

    slingscold and paperapps

--- config.def.h
+++ config.def.h
@@ -77,13 +77,17 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {"dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL};
 static const char *termcmd[] = {"st", NULL};
+static const char *papercmd[] = {"paperapps", NULL};
 static const char *slockcmd[] = {"slock", NULL};
 static const char  *rangercmd[] = { "st", "-e", "sh", "-c", "ranger", NULL };
+static const char *slingscold[] = { "slingscold-launcher", NULL};
 #include "push.c"
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{MODKEY, XK_r, spawn, {.v = rangercmd } },
+	{MODKEY, XK_y, spawn, {.v = slingscold} },
+	{MODKEY, XK_a, spawn, {.v = papercmd} },
 	{MODKEY, XK_p, spawn, {.v = dmenucmd}},
 	{MODKEY | ControlMask, XK_l, spawn, {.v = slockcmd}},
 	{MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
