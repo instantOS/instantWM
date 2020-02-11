ac8a5989d2601ffc8bba21e2adb59fa8c8f24045
paperbenni <paperbenni@gmail.com>
2020-02-11 12:40:25 +0100

    remove combotag and change prefix

--- config.def.h
+++ config.def.h
@@ -10,12 +10,12 @@ static const int showsystray = 1;			  /* 0 means no systray */
 static const int showbar = 1;				  /* 0 means no bar */
 static const int topbar = 1;				  /* 0 means bottom bar */
 static const char *fonts[] = {"Cantarell-Regular:size=12"};
-static const char col_gray1[] = "#1B2224"; /* top bar d */
-static const char col_gray2[] = "#A4ABAA";/*unfocused fonts d */
-static const char col_gray3[] = "#686868";/*unfocused border d */
-static const char col_gray4[] = "#FFFFFF";/*focused fonts d */
-static const char col_gray5[] = "#2EB398";/*focused windows d */
-static const char col_cyan[] = "#2EB398";/*focused instantmenu or topbar d */
+static const char col_gray1[] = "#292f3a"; /* top bar d */
+static const char col_gray2[] = "#ffffff";/*unfocused fonts d */
+static const char col_gray3[] = "#747c90";/*unfocused border d */
+static const char col_gray4[] = "#ffffff";/*focused fonts d */
+static const char col_gray5[] = "#4dadd4";/*focused windows d */
+static const char col_cyan[] = "#5294E2";/*focused instantmenu or topbar d */
 static const char *colors[][3] = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray2, col_gray1, col_gray3 },
@@ -63,9 +63,9 @@ static const Layout layouts[] = {
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY, TAG)                                          \
-	{MODKEY, KEY, comboview, {.ui = 1 << TAG}},                         \
+	{MODKEY, KEY, view, {.ui = 1 << TAG}},                         \
 		{MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}}, \
-		{MODKEY | ShiftMask, KEY, combotag, {.ui = 1 << TAG}},          \
+		{MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},          \
 		{MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
 
 
