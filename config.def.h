20f2b3381a04ecbcfdf25f68123f08a08b448e76
paperbenni <paperbenni@gmail.com>
2020-03-08 18:19:36 +0100

    scroll to change tags

--- config.def.h
+++ config.def.h
@@ -187,6 +187,8 @@ static Button buttons[] = {
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
+	{ ClkTagBar,            0,              Button5,        viewtoright,    {0} },
+	{ ClkTagBar,            0,              Button4,        viewtoleft,     {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
