ed8d7733de1f79277230826c4a2bd7d219c89300
paperbenni <paperbenni@gmail.com>
2020-03-25 11:04:02 +0100

    auto start with monocle layout to prevent crash loops

--- instantwm.c
+++ instantwm.c
@@ -967,8 +967,8 @@ createmon(void)
 	m->nmaster = nmaster;
 	m->showbar = showbar;
 	m->topbar = topbar;
-	m->lt[0] = &layouts[0];
-	m->lt[1] = &layouts[1 % LENGTH(layouts)];
+	m->lt[0] = &layouts[3];
+	m->lt[1] = &layouts[0];
 	strncpy(m->ltsymbol, layouts[0].symbol, sizeof m->ltsymbol);
 	m->pertag = ecalloc(1, sizeof(Pertag));
 	m->pertag->curtag = m->pertag->prevtag = 1;
@@ -977,8 +977,8 @@ createmon(void)
 		m->pertag->nmasters[i] = m->nmaster;
 		m->pertag->mfacts[i] = m->mfact;
 
-		m->pertag->ltidxs[i][0] = m->lt[0];
-		m->pertag->ltidxs[i][1] = m->lt[1];
+		m->pertag->ltidxs[i][0] = m->lt[1];
+		m->pertag->ltidxs[i][1] = m->lt[0];
 		m->pertag->sellts[i] = m->sellt;
 
 		m->pertag->showbars[i] = m->showbar;
