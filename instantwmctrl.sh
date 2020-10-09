928dbe9f83976c2555e9bff264b152001dcce7ff
con-f-use <con-f-use@users.noreply.github.com>
2020-10-09 16:02:04 +0200

    shorten layout logic for wmctrl

--- instantwmctrl.sh
+++ instantwmctrl.sh
@@ -1,43 +1,22 @@
-#!/usr/bin/env dash
+#!/usr/bin/env bash
 
 # really basic tool to send commands to instantWM
 
 case $1 in
 layout)
-    if [ "$2" -eq "$2" ]; then
-        LAYOUT="$2"
-    else
-        case $2 in
-        tile)
-            LAYOUT=0
-            ;;
-        grid)
-            LAYOUT=1
-            ;;
-        float)
-            LAYOUT=2
-            ;;
-        monocle)
-            LAYOUT=3
-            ;;
-        tcl)
-            LAYOUT=4
-            ;;
-        deck)
-            LAYOUT=5
-            ;;
-        overviewlayout)
-            LAYOUT=6
-            ;;
-        bstack)
-            LAYOUT=7
-            ;;
-        bstackhoriz)
-            LAYOUT=8
-            ;;
-        esac
-    fi
-    xsetroot -name "c;:;layout;$LAYOUT"
+	if [[ $2 =~ ^[0-9]$ ]]; then
+		layout=$2
+	else
+		declare -A layouts=(
+			["tile"]=0 ['grid']=1 ['float']=2 
+            ['monocle']=2 ['tcl']=4 ['deck']=5
+			['overview']=6 ['bstack']=7 ['bstackhoriz']=8
+		)
+		layout=${layouts[$2]}
+		[ -z "$layout" ] &&
+		  { echo "Error: Unknown layout '$2'"; exit 1; }
+	fi 
+    xsetroot -name "c;:;layout;$layout"
     exit
     ;;
 esac
