3ba82963c524f925f5a2ac1608edcc55e334986d
con-f-use <con-f-use@users.noreply.github.com>
2020-10-09 16:09:04 +0200

    fix formatting in wmctrl

--- instantwmctrl.sh
+++ instantwmctrl.sh
@@ -4,18 +4,18 @@
 
 case $1 in
 layout)
-	if [[ $2 =~ ^[0-9]$ ]]; then
-		layout=$2
-	else
-		declare -A layouts=(
-			["tile"]=0 ['grid']=1 ['float']=2 
-            ['monocle']=2 ['tcl']=4 ['deck']=5
-			['overview']=6 ['bstack']=7 ['bstackhoriz']=8
-		)
-		layout=${layouts[$2]}
-		[ -z "$layout" ] &&
-		  { echo "Error: Unknown layout '$2'"; exit 1; }
-	fi 
+    if [[ $2 =~ ^[0-8]$ ]]; then # between zero and eight?
+        layout=$2
+    else
+        declare -A layouts=(
+          ["tile"]=0 ['grid']=1 ['float']=2 
+          ['monocle']=2 ['tcl']=4 ['deck']=5
+          ['overview']=6 ['bstack']=7 ['bstackhoriz']=8
+        )
+        layout=${layouts[$2]}
+        [ -z "$layout" ] &&
+          { echo "Error: Unknown layout '$2'"; exit 1; }
+    fi 
     xsetroot -name "c;:;layout;$layout"
     exit
     ;;
