11847f0eb23189c8fc18ec2be52160b342f860dc
con-f-use <con-f-use@gmx.net>
2020-11-11 23:30:19 +0100

    ctrl: layout in sourcable function

--- instantwmctrl.sh
+++ instantwmctrl.sh
@@ -10,25 +10,25 @@ Commands:
 main() {
     case $1 in
         help) usage -h ;;
-
-        layout)
-            if [[ $2 =~ ^[0-8]$ ]]; then # between zero and eight?
-                layout=$2
-            else
-                declare -A layouts=(
-                ["tile"]=0 ['grid']=1 ['float']=2 
-                ['monocle']=3 ['tcl']=4 ['deck']=5
-                ['overview']=6 ['bstack']=7 ['bstackhoriz']=8
-                )
-                layout=${layouts[$2]}
-                [ -z "$layout" ] &&
-                { echo "Error: Unknown layout '$2'"; exit 1; }
-            fi 
-            xsetroot -name "c;:;layout;$layout"
-            exit
-        ;;
+        layout) layout $2; exit ;;
     esac
     xsetroot -name "c;:;$1;$2"
+}
+
+layout() {
+    if [[ $1 =~ ^[0-8]$ ]]; then # between zero and eight
+        layout=$1
+    else
+        declare -A layouts=(
+            ["tile"]=0 ['grid']=1 ['float']=2 
+            ['monocle']=3 ['tcl']=4 ['deck']=5
+            ['overview']=6 ['bstack']=7 ['bstackhoriz']=8
+        )
+        layout=${layouts[$1]}
+        [ -z "$layout" ] &&
+            { echo "Error: Unknown layout '$1'"; exit 1; }
+    fi 
+    xsetroot -name "c;:;layout;$layout"
 }
 
 usage() {
