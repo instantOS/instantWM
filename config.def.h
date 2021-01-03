44dab67ea8c769a3191576fc63f418bb10037b46
ppenguin <hieronymusv@gmail.com>
2021-01-03 14:31:50 +0100

    Update config.def.h: added a few floating window classes

--- config.def.h
+++ config.def.h
@@ -303,12 +303,9 @@ static const Rule rules[] = {
 	{"org-wellkord-globonote-Main", NULL,     NULL,        0,            1,           -1},
 	{"Peek", NULL,     NULL,        0,            1,           -1},
 	{"ROX-Filer", NULL,     NULL,        0,            0,           -1},
-	{"QjackCtl", NULL,     NULL,       0,            1,           -1},
-	{"KeePassXC", NULL,     NULL,       0,            1,           -1},
-	{"Ardour", NULL,     NULL,       0,            1,           -1},
-	{"VirtualBox", NULL,     NULL,       0,            1,           -1},
-	{"wechat", NULL,     NULL,       0,            1,           -1},
-	{"whatsapp", NULL,     NULL,       0,            1,           -1},
+	{"qjackctl", NULL,     NULL,       0,            1,           -1},
+	{"keepassxc", NULL,     NULL,       0,            1,           -1},
+	{"ardour-6.5.0", NULL,     NULL,       0,            1,           -1},
 };
 
 /* layout(s) */
