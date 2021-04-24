a0e449f360f0ab2bf6b8f2d3721c7b3364c862c2
con-f-use <con-f-use@users.noreply.github.com>
2021-04-24 12:53:04 +0200

    more forgiving build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,15 @@
 
 # compile and install instantWM
 
+SUPERTOOL="sudo"
+if [[ -x /usr/bin/doas ]] && [[ -s /etc/doas.conf ]] ; then
+    SUPERTOOL="doas"
+fi
+
 make clean &>/dev/null
 
 if [ -z "$2" ]; then
-    rm config.h &>/dev/null
-    make && sudo make install
+    mv config.h "$(mktemp --tmpdir instantwmcfg_XXXX.h)" &>/dev/null && 
+        echo "Existing config.h moved to /tmp/" 1>&2
+    $SUPERTOOL make install
 fi
