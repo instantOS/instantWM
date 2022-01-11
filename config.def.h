6314d3ab935a80c62a5d4aec99426ec81a722474
Saphira Kai <kai.saphira@gmail.com>
2022-01-11 15:53:01 -0400

    fix Ubuntu Light font

--- config.def.h
+++ config.def.h
@@ -12,7 +12,7 @@ static const int systraypinningfailfirst = 1; /* 1: if pinning fails, display sy
 static const int showsystray = 1;			  /* 0 means no systray */
 static const int showbar = 1;				  /* 0 means no bar */
 static const int topbar = 1;				  /* 0 means bottom bar */
-static const char *fonts[] = {"Ubuntu Light:size=12", "Cascadia Code:style=SemiLight:size=12"};
+static const char *fonts[] = {"Ubuntu:size=12:style=Light", "Cascadia Code:style=SemiLight:size=12"};
 
 static int barheight;
 static char xresourcesfont[30];
