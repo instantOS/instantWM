d3364751c86da9d9fc3af214efa2d525f24d6f3d
paperbenni <paperbenni@gmail.com>
2022-08-07 11:26:38 +0200

    pull upstream until cd0773cee9bad694dc9a6b1355a32bbe61abadff

--- config.mk
+++ config.mk
@@ -20,6 +20,7 @@ FREETYPELIBS = -lfontconfig -lXft
 FREETYPEINC = /usr/include/freetype2
 # OpenBSD (uncomment)
 #FREETYPEINC = ${X11INC}/freetype2
+#MANPREFIX = ${PREFIX}/man
 
 # includes and libs
 INCS = -I${X11INC} -I${FREETYPEINC}
