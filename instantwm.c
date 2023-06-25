ab22ce0b30884213109c20e03fd5d3d513e3056e
paperbenni <paperbenni@gmail.com>
2023-06-25 12:04:10 +0200

    merge upstream until e81f17d4c196aaed6893fd4beed49991caa3e2a4

--- instantwm.c
+++ instantwm.c
@@ -4623,12 +4623,17 @@ void showhide(Client *c) {
 }
 
 void spawn(const Arg *arg) {
+    struct sigaction sa;
     if (arg->v == instantmenucmd)
         instantmenumon[0] = '0' + selmon->num;
     if (fork() == 0) {
         if (dpy)
             close(ConnectionNumber(dpy));
         setsid();
+        sigemptyset(&sa.sa_mask);
+        sa.sa_flags = 0;
+        sa.sa_handler = SIG_DFL;
+        sigaction(SIGCHLD, &sa, NULL);
         execvp(((char **)arg->v)[0], (char **)arg->v);
         die("instantwm: execvp '%s' failed:", ((char **)arg->v)[0]);
     }
