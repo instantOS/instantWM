2df510572af5771ce87e5f4eedef829a5511f328
paperbenni <paperbenni@gmail.com>
2026-02-20 19:51:07 +0100

    init rust lib

---
+++ rust/src/main.rs
@@ -0,0 +1,10 @@
+use instantwm::types::*;
+
+fn main() {
+    println!("instantWM - Rust port");
+
+    // TODO: Initialize X11 display
+    // TODO: Set up error handlers
+    // TODO: Initialize window manager
+    // TODO: Enter main event loop
+}
